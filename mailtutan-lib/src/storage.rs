0a5aa39014e31a652914519f2c7832f228e0b1e0
Mohsen Alizadeh <mohsen@alizadeh.us>
2023-05-21 15:35:27 -0700

    impl Storage for Memory

--- mailtutan-lib/src/storage.rs
+++ mailtutan-lib/src/storage.rs
@@ -22,14 +22,57 @@ impl Memory {
     }
 }
 
+impl Storage for Memory {
+    fn list(&self) -> &Vec<Message> {
+        &self.records
+    }
+
+    fn add(&mut self, mut message: Message) -> usize {
+        message.id = Some(self.sequence_id);
+
+        self.sequence_id += 1;
+
+        let event = MessageEvent {
+            event_type: "add".to_owned(),
+            message: message.clone(),
+        };
+
+        // TODO: publish the message
+        // WEBSOCKET_TX
+        //     .clone()
+        //     .send(serde_json::to_string(&event).unwrap())
+        //     .ok();
+
+        self.records.push(message);
+
+        self.sequence_id
+    }
+
+    fn get(&self, item: usize) -> &Message {
+        &self.records.get(item - 1).unwrap()
+    }
+
+    #[allow(dead_code)]
+    fn size(&self) -> usize {
+        self.records.len()
+    }
+
+    fn delete_all(&mut self) {
+        self.records.clear()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{models::Message, store::MemoryStore};
+    use super::Memory;
+    use super::Storage;
+    use crate::models::Message;
     use std::assert_eq;
 
     #[test]
     fn test_store() {
-        let mut store = MemoryStore::new();
+        let mut store = Memory::new();
+
         store.add(Message {
             ..Default::default()
         });
