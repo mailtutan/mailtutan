86d74d711538b0c0126f44c4835cb948276b9bce
Mohsen Alizadeh <mohsen@alizadeh.us>
2023-05-30 21:16:57 -0700

    change theme

--- mailtutan-web/src/component/header.rs
+++ mailtutan-web/src/component/header.rs
@@ -7,7 +7,7 @@ pub fn Header() -> Html {
         <h1>{ "Mailtutan" }</h1>
         <nav class="app">
           <ul>
-            <li class="search"><input type="search" name="search" placeholder="Search messages..." incremental="true" /></li>
+            <li class="search hidden"><input type="search" name="search" placeholder="Search messages..." incremental="true" /></li>
             <li class="clear"><a href="#" title="Clear all messages">{ "Clear" }</a></li>
               <li class="quit"><a href="#" title="Quit Mailtutan">{ "Quit" }</a></li>
           </ul>
