fe834231c74bfc443819cad3cb94d18d37e97150
Geert Stappers <stappers@stappers.it>
2023-06-17 16:14:05 +0200

    GET api/version returns JSON
    Besides
      -use axum::response::Html;
      +use axum::response::Json;
    was also
      +pub struct VersionInfo
    needed.
    
    Signed-off-by: Geert Stappers <stappers@stappers.it>

--- mailtutan-lib/src/api/version.rs
+++ mailtutan-lib/src/api/version.rs
@@ -1,5 +1,14 @@
-use axum::response::Html;
+use axum::response::Json;
+use serde::Serialize;
 
-pub async fn show() -> Html<&'static str> {
-    Html(env!("CARGO_PKG_VERSION"))
+pub async fn show() -> Json<VersionInfo> {
+    let vi = VersionInfo {
+        version: env!("CARGO_PKG_VERSION"),
+    };
+    Json(vi)
+}
+
+#[derive(Debug, Serialize)]
+pub struct VersionInfo {
+    version: &'static str,
 }
