c2e3e2696cd1e404645e04734f0ed94057d9be72
Mohsen Alizadeh <mohsen@alizadeh.us>
2023-06-17 14:49:55 -0700

    #10 support tls

--- mailtutan-lib/src/smtp.rs
+++ mailtutan-lib/src/smtp.rs
@@ -1,7 +1,7 @@
 use crate::models::Message;
 use crate::models::MessageEvent;
 use crate::APP;
-use mailin_embedded::{Handler, Server};
+use mailin_embedded::{Handler, Server, SslConfig};
 use std::io;
 
 #[derive(Clone)]
@@ -11,14 +11,12 @@ struct MyHandler {
 
 impl Handler for MyHandler {
     fn data(&mut self, buf: &[u8]) -> io::Result<()> {
-        // dbg!("data received");
         self.data.append(&mut buf.to_owned());
 
         Ok(())
     }
 
     fn data_end(&mut self) -> mailin_embedded::Response {
-        // dbg!("data end");
         let message = Message::from(&self.data);
 
         let msg = APP
@@ -50,10 +48,28 @@ impl Handler for MyHandler {
 pub async fn serve() {
     let handler = MyHandler { data: vec![] };
     let mut server = Server::new(handler);
-
     let uri = APP.get().unwrap().lock().unwrap().get_smtp_uri();
 
-    server.with_addr(&uri).unwrap();
+    let ssl: SslConfig = {
+        let app = APP.get().unwrap().lock().unwrap();
+
+        if let (Some(cert_path), Some(key_path)) =
+            (app.smtp_cert_path.clone(), app.smtp_key_path.clone())
+        {
+            SslConfig::SelfSigned {
+                cert_path: cert_path,
+                key_path: key_path,
+            }
+        } else {
+            SslConfig::None
+        }
+    };
+
+    server
+        .with_ssl(ssl)
+        .expect("SslConfig error")
+        .with_addr(&uri)
+        .unwrap();
 
     println!("listening on smtp://{}", &uri);
 
